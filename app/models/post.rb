89cd9822c63546bce812760797d3d4f7049ae1f0
cpstrillvt93 <cpsvt93@gmail.com>
2017-03-30 11:09:54 -0400

    Add validations and null false to post attributes in model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   validates :user, presence: true
+  validates :title, presence: true
+  validates :body, presence: true
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -24,8 +24,8 @@ ActiveRecord::Schema.define(version: 20170327160725) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
+    t.string   "title",      null: false
+    t.text     "body",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
